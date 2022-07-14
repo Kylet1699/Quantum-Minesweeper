6d122ba9cc1da817da33f73b5f13ce83787b2360
Tony Ma <tonyphma@gmail.com>
2022-07-13 18:12:28 -0700

    Show (potential) neighbouring bombs on cleared tile

--- QuantumMinesweeper.py
+++ QuantumMinesweeper.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def main():
     # Takes in input from user
-    GRID_SIZE = int(input('Please enter a size: '))
+    size = int(input('Please enter a size: '))
     # Initialize the board and make a copy of it, solution board and play board
-    revealed = initialize(GRID_SIZE)
+    revealed = initialize(size)
     grid = np.copy(revealed)
 
     for x in range(len(grid)):
@@ -17,7 +17,7 @@ def main():
                 grid[x][y] = '-'
 
     tui.draw(revealed)
-    play(grid, revealed)
+    play(grid, revealed, size)
 
 # Populate the board with bomb_tester results
 def initialize(grid_size):
@@ -29,8 +29,19 @@ def initialize(grid_size):
             bomb_grid[x][y] = bt.get_count(6)[0] # Returns a string of qubits
     return bomb_grid
 
+# Count the number of neighbouring bombs (predicted)
+def get_neighbours(x, y, size, grid):
+    count = 0
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if (i >= 0 and j >= 0 and i < size and j < size
+                and grid[i][j][0] == '1' and not (x == i and y == j)):
+                count += 1
+
+    return count
+
 # Game logic
-def play(grid, solution):
+def play(grid, solution, size):
     while True:
         tui.draw(grid)
         try:
@@ -49,13 +60,13 @@ def play(grid, solution):
             # Continue the game if user picks right, otherwise exit
             if solution[x][y][0] == selection:
                 print('Congratz, you predicted correctly')
-                grid[x][y] = '◻'
+                grid[x][y] = get_neighbours(x, y, size, solution)
             else:
                 print('You lose')
-                break;
+                break
 
-        except:
-            print('Invalid input.')
+        except Exception as e:
+            print('Error:', e)
             break
 
 if __name__ == '__main__':
