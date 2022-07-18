bbd6d2e03230287a13c7419fb3cae2e5be56f8ac
wanyingc <wanying.cwy@gmail.com>
2022-07-18 15:11:42 -0700

    Modified set_difficulty

--- test.py
+++ test.py
@@ -8,15 +8,9 @@ def btn_click(i):
 
 # set size of grid according to the chosen difficulty option
 def set_difficulty(diffOption):
-    if diffOption == "easy":
-        SIZE[0] = 4
-    elif diffOption == "medium":
-        SIZE[0] = 6
-    elif diffOption == "hard":
-        SIZE[0] = 8
+    SIZE[0] = diffOption
 
     print(SIZE[0])
-    draw(SIZE[0])
 
 
 def draw(grid):
@@ -39,9 +33,9 @@ menubar = tk.Menu(window)
 window.config(menu=menubar)
 
 difficulty_menu = tk.Menu(menubar)
-difficulty_menu.add_command(label='Easy', command=lambda diffOption="easy": set_difficulty(diffOption))
-difficulty_menu.add_command(label='Medium', command=lambda diffOption="medium": set_difficulty(diffOption))
-difficulty_menu.add_command(label='Hard', command=lambda diffOption="hard": set_difficulty(diffOption))
+difficulty_menu.add_command(label='Easy', command=lambda diffOption=4: set_difficulty(diffOption))
+difficulty_menu.add_command(label='Medium', command=lambda diffOption=6: set_difficulty(diffOption))
+difficulty_menu.add_command(label='Hard', command=lambda diffOption=8: set_difficulty(diffOption))
 
 menubar.add_cascade(label="Choose Difficulty", menu=difficulty_menu, underline=0)
 
